3bb78f391bfb5cb77ba8ca848952e5357bada8ec
Greg Kurtzer <GMKurtzer@lbl.gov>
2012-05-15 22:18:57 +0000

    3.2 release of common, provision, vnfs and ipmi
    git-svn-id: https://warewulf.lbl.gov/svn/branches/common@972 3fa55bd7-8392-0410-b022-a3fc164e7bfc

---
+++ 3.2/lib/Warewulf/Event/NewObject.pm
@@ -0,0 +1,47 @@
+# Copyright (c) 2001-2003 Gregory M. Kurtzer
+#
+# Copyright (c) 2003-2011, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory (subject to receipt of any
+# required approvals from the U.S. Dept. of Energy).  All rights reserved.
+#
+#
+# $Id$
+#
+
+package Warewulf::Event::NewObject;
+
+use Warewulf::Logger;
+use Warewulf::Event;
+use Warewulf::EventHandler;
+use Warewulf::Config;
+use Warewulf::DataStore;
+
+my $event = Warewulf::EventHandler->new();
+
+sub
+default_config()
+{
+    my @objects = @_;
+    my $db = Warewulf::DataStore->new();
+
+    &iprint("Building default configuration for new object(s)\n");
+
+    foreach my $obj (@objects) {
+        my $type = $obj->type();
+        my $def_object = $db->get_objects($type, "name", "DEFAULT")->get_object(0);
+        if ($def_object) {
+            my %hash = $def_object->get_hash();
+            foreach my $key (keys %hash) {
+                if (! $obj->get($key)) {
+                    $obj->set($key, $hash{"$key"});
+                }
+            }
+        }
+    }
+
+}
+
+
+$event->register("*.new", \&default_config);
+
+1;
