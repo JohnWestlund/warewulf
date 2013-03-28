c9cbbf1174bb5d168437e056afc873106acfe501
Matias Cambral <matias.a.cambral@intel.com>
2013-03-28 20:57:22 +0000

    wwinit mic first version. Updated icr overlay and  icr version
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@1342 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- common/lib/Warewulf/Module/Cli/Node.pm
+++ common/lib/Warewulf/Module/Cli/Node.pm
@@ -5,8 +5,6 @@
 # through Lawrence Berkeley National Laboratory (subject to receipt of any
 # required approvals from the U.S. Dept. of Energy).  All rights reserved.
 #
-
-
 package Warewulf::Module::Cli::Node;
 
 use Warewulf::Config;
