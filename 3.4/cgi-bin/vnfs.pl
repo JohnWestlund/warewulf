7d83efa0f49304709206c385ed7960e6e2f421b2
Greg Kurtzer <GMKurtzer@lbl.gov>
2013-04-10 20:49:06 +0000

    Creating branches for the 3.4 release of cluster, common, ipmi, provision and vnfs.
    git-svn-id: https://warewulf.lbl.gov/svn/branches/provision@1359 3fa55bd7-8392-0410-b022-a3fc164e7bfc

---
+++ 3.4/cgi-bin/vnfs.pl
@@ -0,0 +1,149 @@
+#!/usr/bin/perl
+#
+# Copyright (c) 2001-2003 Gregory M. Kurtzer
+#
+# Copyright (c) 2003-2011, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory (subject to receipt of any
+# required approvals from the U.S. Dept. of Energy).  All rights reserved.
+#
+
+
+use CGI;
+use Warewulf::Util;
+use Warewulf::DataStore;
+use Warewulf::Logger;
+use Warewulf::Daemon;
+use Warewulf::Node;
+use Warewulf::Vnfs;
+use Warewulf::Provision;
+use File::Path;
+use File::Basename;
+
+&daemonized(1);
+&set_log_level("WARNING");
+
+my $q = CGI->new();
+my $db = Warewulf::DataStore->new();
+
+my $vnfs_cachedir = "/var/tmp/warewulf_cache/";
+
+
+if ($q->param('hwaddr')) {
+    my $hwaddr = $q->param('hwaddr');
+    if ($hwaddr =~ /^([a-zA-Z0-9:]+)$/) {
+        my $hwaddr = $1;
+        my $nodeSet = $db->get_objects("node", "_hwaddr", $hwaddr);
+        my $node = $nodeSet->get_object(0);
+        if ($node) {
+            my ($node_name) = $node->name();
+            my ($vnfsid) = $node->vnfsid();
+            if ($vnfsid) {
+                my $obj = $db->get_objects("vnfs", "_id", $vnfsid)->get_object(0);
+                if ($obj) {
+                    my $vnfs_name = $obj->name();
+                    my $vnfs_checksum = $obj->checksum();
+                    my ($vnfs_nocache) = $obj->get("nocache");
+                    my $use_cache;
+
+                    #&nprint("Sending VNFS '$vnfs_name' to node '$node_name'\n");
+                    $q->print("Content-Type: application/octet-stream; name=\"vnfs.img\"\r\n");
+                    if (my $size = $obj->size()) {
+                        $q->print("Content-length: $size\r\n");
+                    }
+                    $q->print("Content-Disposition: attachment; filename=\"vnfs.img\"\r\n");
+                    $q->print("\r\n");
+
+                    if (! $vnfs_nocache) {
+                        if (-f "$vnfs_cachedir/$vnfs_name/image.$vnfs_checksum") {
+                            &dprint("Found VNFS cache\n");
+                            # Perhaps do a better job checking here... one day.
+                            $use_cache = 1;
+                        } else {
+                            &dprint("Building VNFS cache\n");
+                            my $rand = &rand_string(8);
+                            my $cache_fh;
+
+                            if (! -d "$vnfs_cachedir/$vnfs_name") {
+                                mkpath("$vnfs_cachedir/$vnfs_name");
+                            }
+                            
+                            open($cache_fh, "> $vnfs_cachedir/$vnfs_name/image.$vnfs_checksum.$rand");
+                            my $binstore = $db->binstore($obj->get("_id"));
+
+                            while(my $buffer = $binstore->get_chunk()) {
+                                print $cache_fh $buffer;
+                            }
+                            if (close($cache_fh)) {
+                                rename("$vnfs_cachedir/$vnfs_name/image.$vnfs_checksum.$rand", "$vnfs_cachedir/$vnfs_name/image.$vnfs_checksum");
+                                foreach my $image (glob("$vnfs_cachedir/$vnfs_name/image.*")) {
+                                    if ($image =~ /^([a-zA-Z0-9\/\-\._]+?\/image\.[a-zA-Z0-9]+)$/) {
+                                        $image = $1;
+                                        my $basename = basename($image);
+                                        if ($basename ne "image.$vnfs_checksum") {
+                                            &wprint("Clearing old vnfs cache: $image\n");
+                                            unlink($image);
+                                        }
+                                    }
+                                }
+                                $use_cache = 1;
+                            }
+                        }
+                    }
+
+                    if ($use_cache) {
+                        &dprint("Sending cached VNFS\n");
+                        my $cache_fh;
+                        if (open($cache_fh, "$vnfs_cachedir/$vnfs_name/image.$vnfs_checksum")) {
+                            my $buffer;
+                            while($buffer = <$cache_fh>) {
+                                $q->print($buffer);
+                            }
+                            close($cache_fh);
+                        } else {
+                            &eprint("Can't open VNFS cache!\n");
+                            $q->print("Content-Type: application/octet-stream\r\n");
+                            $q->print("Status: 500\r\n");
+                            $q->print("\r\n");
+                        }
+
+                    } else {
+                        &dprint("Sending VNFS from the data store\n");
+                        my $binstore = $db->binstore($obj->get("_id"));
+                        while(my $buffer = $binstore->get_chunk()) {
+                            $q->print($buffer);
+                        }
+                    }
+
+                } else {
+                    &eprint("VNFS request for an unset VNFS\n");
+                    $q->print("Content-Type: application/octet-stream\r\n");
+                    $q->print("Status: 404\r\n");
+                    $q->print("\r\n");
+                }
+            } else {
+                &eprint($node->get("name") ." has no VNFS set\n");
+                $q->print("Content-Type: application/octet-stream\r\n");
+                $q->print("Status: 404\r\n");
+                $q->print("\r\n");
+            }
+        } else {
+            &eprint("VNFS request for an unknown node\n");
+            $q->print("Content-Type: application/octet-stream\r\n");
+            $q->print("Status: 404\r\n");
+            $q->print("\r\n");
+        }
+    } else {
+        &eprint("VNFS request for a bad hwaddr\n");
+        $q->print("Content-Type: application/octet-stream\r\n");
+        $q->print("Status: 404\r\n");
+        $q->print("\r\n");
+    }
+} else {
+    &eprint("VNFS request without a hwaddr\n");
+    $q->print("Content-Type: application/octet-stream\r\n");
+    $q->print("Status: 404\r\n");
+    $q->print("\r\n");
+}
+
+
+
---
+++ 3.4/lib/Warewulf/Provision.pm
@@ -0,0 +1,288 @@
+# Copyright (c) 2001-2003 Gregory M. Kurtzer
+#
+# Copyright (c) 2003-2011, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory (subject to receipt of any
+# required approvals from the U.S. Dept. of Energy).  All rights reserved.
+#
+#
+
+package Warewulf::Provision;
+
+use Warewulf::Object;
+use Warewulf::Logger;
+use Warewulf::DataStore;
+use Warewulf::Util;
+use Warewulf::Node;
+
+our @ISA = ('Warewulf::Object');
+
+push(@Warewulf::Node::ISA, 'Warewulf::Provision');
+
+=head1 NAME
+
+Warewulf::Provision - Provision object class for Warewulf
+
+=head1 ABOUT
+
+Object class for extending the Node objects for provisioning.
+
+=head1 SYNOPSIS
+
+    use Warewulf::Node;
+    use Warewulf::Provision;
+
+    my $obj = Warewulf::Node->new();
+
+=head1 METHODS
+
+=over 12
+
+=cut
+
+
+
+=item bootstrapid($string)
+
+Set or return the bootstrap ID for this node
+
+=cut
+
+sub
+bootstrapid()
+{
+    my $self = shift;
+
+    return $self->prop("bootstrapid", qr/^([0-9]+)$/, @_);
+}
+
+
+=item vnfsid($string)
+
+Set or return the VNFS ID for this node
+
+=cut
+
+sub
+vnfsid()
+{
+    my $self = shift;
+
+    return $self->prop("vnfsid", qr/^([0-9]+)$/, @_);
+}
+
+=item vnfs($string)
+
+Return the name of the VNFS this node is configured to use
+
+=cut
+
+sub
+vnfs()
+{
+    my $self = shift;
+
+
+}
+
+
+=item fileids(@fileids)
+
+Set or return the list of file ID's to be provisioned for this node
+
+=cut
+
+sub
+fileids()
+{
+    my ($self, @strings) = @_;
+    my $key = "fileids";
+
+    if (@strings) {
+        my $name = $self->get("name");
+        my @new;
+        foreach my $string (@strings) {
+            if ($string =~ /^([0-9]+)$/) {
+                &dprint("Object $name set $key += '$1'\n");
+                push(@new, $1);
+            } else {
+                &eprint("Invalid characters to set $key += '$string'\n");
+            }
+            $self->set($key, @new);
+        }
+    }
+
+    return($self->get($key));
+}
+
+=item kargs()
+
+Set or return the list of kernel arguments. If an array element
+includes whitespace (i.e. it includes multiple kernel arguments),
+split it and store it as separate array elements.
+
+=cut
+
+sub 
+kargs()
+{
+    my ($self, @strings) = @_;
+    my $key = "kargs";
+
+    if (uc($strings[0]) eq "UNDEF") {
+        $self->del($key);
+        return $self->get($key);
+    }
+
+    if (@strings) {
+        my $name = $self->get("name");
+        my @new;
+        foreach my $string (@strings) {
+            my @kargs = split(/\s+/,$string); # pre-emptively split
+            foreach my $karg (@kargs) {
+                &dprint("Object $name set $key +=' $karg'\n");
+                push(@new,$karg);
+            }
+        }
+        $self->set($key,@new);
+    }
+
+    return $self->get($key);
+}
+
+=item fileidadd(@fileids)
+
+Add a file ID or list of file IDs to the current object.
+
+=cut
+
+sub
+fileidadd()
+{
+    my ($self, @strings) = @_;
+    my $key = "fileids";
+
+    if (@strings) {
+        my $name = $self->get("name");
+        foreach my $string (@strings) {
+            if ($string =~ /^([0-9]+)$/) {
+                &dprint("Object $name set $key += '$1'\n");
+                $self->add($key, $1);
+            } else {
+                &eprint("Invalid characters to set $key += '$string'\n");
+            }
+        }
+    }
+
+    return($self->get($key));
+}
+
+
+=item fileiddel(@fileids)
+
+Delete a file ID or list of file IDs to the current object.
+
+=cut
+
+sub
+fileiddel()
+{
+    my ($self, @strings) = @_;
+    my $key = "fileids";
+
+    if (@strings) {
+        my $name = $self->get("name");
+        $self->del($key, @strings);
+        &dprint("Object $name del $key -= @strings\n");
+    }
+
+    return($self->get($key));
+}
+
+
+=item preshell($bool)
+
+Set or return the preshell boolean
+
+=cut
+
+sub
+preshell()
+{
+    my ($self, $bool) = @_;
+
+    if (defined($bool)) {
+        if ($bool) {
+            $self->set("preshell", 1);
+        } else {
+            $self->del("preshell");
+        }
+    }
+
+    return $self->get("preshell");
+}
+
+
+=item postshell($bool)
+
+Set or return the postshell boolean
+
+=cut
+
+sub
+postshell()
+{
+    my ($self, $bool) = @_;
+
+    if (defined($bool)) {
+        if ($bool) {
+            $self->set("postshell", 1);
+        } else {
+            $self->del("postshell");
+        }
+    }
+
+    return $self->get("postshell");
+}
+
+
+=item bootlocal($bool)
+
+Set or return the bootlocal boolean
+
+=cut
+
+sub
+bootlocal()
+{
+    my ($self, $bool) = @_;
+
+    if (defined($bool)) {
+        if ($bool) {
+            $self->set("bootlocal", 1);
+        } else {
+            $self->del("bootlocal");
+        }
+    }
+
+    return $self->get("bootlocal");
+}
+
+
+=back
+
+=head1 SEE ALSO
+
+Warewulf::Object Warewulf::DSO::Node
+
+=head1 COPYRIGHT
+
+Copyright (c) 2001-2003 Gregory M. Kurtzer
+
+Copyright (c) 2003-2011, The Regents of the University of California,
+through Lawrence Berkeley National Laboratory (subject to receipt of any
+required approvals from the U.S. Dept. of Energy).  All rights reserved.
+
+=cut
+
+
+1;
