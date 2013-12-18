JohnWestlund/warewulf

commit 3eb1c368339e594ce18abefd3227af8bcb91312a

Thu Dec 12 14:23:56-0600 2013                       Jason Stover (jms)
Module/Cli/Object.pm:
* Move the line pushing changes onto @changes into the foreach loop
where $obj is defined.
* Change help to show that '-D' is the short option for deleting a
key from an object

----------------------------------------------------------------------  


git-svn-id: https://warewulf.lbl.gov/svn/trunk@1502 3fa55bd7-8392-0410-b022-a3fc164e7bfc

common/lib/Warewulf/Module/Cli/Object.pm
@@ -70,7 +70,7 @@ help()
     $h .= "     -p, --print     Specify which fields are printed (\":all\" for all)\n";
     $h .= "     -s, --set       Set a member variable (or \"field\")\n";
     $h .= "     -a, --add       Add value(s) to specified member array variable\n";
-    $h .= "     -d, --del       Delete value(s) from specified member variable\n";
+    $h .= "     -D, --del       Delete value(s) from specified member variable\n";
     $h .= "\n";
     $h .= "EXAMPLES:\n";
     $h .= "     Warewulf> object print -p :all\n";
@@ -247,10 +247,10 @@ exec()
             }
             @vals = &quotewords(',', 0, $val);
             $val = sprintf("\"%s\"", join("\", \"", @vals));
-            push(@changes, sprintf("%8s: %-20s = %s -> %s\n", "SET", $var,
-                                   $obj->get($var) || "UNDEF", $val || "UNDEF"));
             foreach my $obj (@objList) {
                 $obj->set($var, ((scalar(@vals)) ? (@vals) : (undef)));
+                push(@changes, sprintf("%8s: %-20s = %s -> %s\n", "SET", $var,
+                                       $obj->get($var) || "UNDEF", $val || "UNDEF"));
             }
         }
         foreach my $chg (@opt_add) {

commit 6704f0121f0955fc69c525c80d9d056159b7062c

Re-fix the problem that r1426 attempted to fix which was re-broken by r1502.  Hopefully it's right this time!
git-svn-id: https://warewulf.lbl.gov/svn/trunk@1510 3fa55bd7-8392-0410-b022-a3fc164e7bfc

common/lib/Warewulf/Module/Cli/Object.pm
@@ -248,9 +248,9 @@ exec()
             @vals = &quotewords(',', 0, $val);
             $val = sprintf("\"%s\"", join("\", \"", @vals));
             foreach my $obj (@objList) {
-                $obj->set($var, ((scalar(@vals)) ? (@vals) : (undef)));
                 push(@changes, sprintf("%8s: %-20s = %s -> %s\n", "SET", $var,
                                        $obj->get($var) || "UNDEF", $val || "UNDEF"));
+                $obj->set($var, ((scalar(@vals)) ? (@vals) : (undef)));
             }
         }
         foreach my $chg (@opt_add) {