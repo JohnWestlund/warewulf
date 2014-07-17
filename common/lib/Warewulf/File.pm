5d2a2dd3aacaf2e4efd5253675fb74e4c0d39d85
Michael Jennings <MEJennings@lbl.gov>
2014-07-17 23:43:54 +0000

    Add support for provisioning file objects which are not regular files (i.e., directories, symlinks, device files, and FIFOs).
    Also some efficiency improvements and cleanups.
    
    This is a big change, so it definitely needs testing! :-)
    
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@1719 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- common/lib/Warewulf/File.pm
+++ common/lib/Warewulf/File.pm
@@ -16,6 +16,7 @@ use Warewulf::EventHandler;
 use File::Basename;
 use File::Path;
 use Digest::MD5;
+use Fcntl ':mode';
 
 our @ISA = ('Warewulf::Object');
 
@@ -100,6 +101,7 @@ mode()
     return $self->prop("mode", sub { return (keys %{ +{$_[0] & 07777,1} })[0] || 0; }, @_);
 }
 
+
 =item modestring()
 
 Returns the file permissions in string form.
@@ -111,30 +113,60 @@ modestring()
 {
     my $self = shift;
     my $mode = $self->mode() || 0;
-    my $str = '-';
+    my $str = "";
 
-    $str .= (($mode & 0400) ? ('r') : ('-'));
-    $str .= (($mode & 0200) ? ('w') : ('-'));
-    if ($mode & 04000) {
-        $str .= (($mode & 0100) ? ('s') : ('S'));
+    $str .= (($mode & S_IRUSR) ? ('r') : ('-'));
+    $str .= (($mode & S_IWUSR) ? ('w') : ('-'));
+    if ($mode & S_ISUID) {
+        $str .= (($mode & S_IXUSR) ? ('s') : ('S'));
     } else {
-        $str .= (($mode & 0100) ? ('x') : ('-'));
+        $str .= (($mode & S_IXUSR) ? ('x') : ('-'));
     }
-    $str .= (($mode & 0040) ? ('r') : ('-'));
-    $str .= (($mode & 0020) ? ('w') : ('-'));
-    if ($mode & 02000) {
-        $str .= (($mode & 0010) ? ('s') : ('S'));
+    $str .= (($mode & S_IRGRP) ? ('r') : ('-'));
+    $str .= (($mode & S_IWGRP) ? ('w') : ('-'));
+    if ($mode & S_ISGID) {
+        $str .= (($mode & S_IXGRP) ? ('s') : ('S'));
     } else {
-        $str .= (($mode & 0010) ? ('x') : ('-'));
+        $str .= (($mode & S_IXGRP) ? ('x') : ('-'));
     }
-    $str .= (($mode & 0004) ? ('r') : ('-'));
-    $str .= (($mode & 0002) ? ('w') : ('-'));
-    if ($mode & 01000) {
-        $str .= (($mode & 0001) ? ('t') : ('T'));
+    $str .= (($mode & S_IROTH) ? ('r') : ('-'));
+    $str .= (($mode & S_IWOTH) ? ('w') : ('-'));
+    if ($mode & S_ISTXT) {
+        $str .= (($mode & S_IXOTH) ? ('t') : ('T'));
     } else {
-        $str .= (($mode & 0001) ? ('x') : ('-'));
+        $str .= (($mode & S_IXOTH) ? ('x') : ('-'));
     }
     return $str;
+}
+
+
+=item filetypestring()
+
+Return the file type in string form
+
+=cut
+
+sub
+filetypestring()
+{
+    my $self = shift;
+    my $ftype = $self->filetype();
+
+    if (S_ISDIR($ftype)) {
+        return 'd';
+    } elsif (S_ISLNK($ftype)) {
+        return 'l';
+    } elsif (S_ISBLK($ftype)) {
+        return 'b';
+    } elsif (S_ISCHR($ftype)) {
+        return 'c';
+    } elsif (S_ISFIFO($ftype)) {
+        return 'p';
+    } elsif (S_ISSOCK($ftype)) {
+        return 's';
+    } else {
+        return '-';
+    }
 }
 
 
@@ -302,21 +334,35 @@ sync()
     if ($self->origin()) {
         my $db = Warewulf::DataStore->new();
         my $binstore = $db->binstore($self->id());
-        my ($total_len, $cur_len, $start) = (0, 0, 0);
-        my ($data, $digest);
+        my ($total_len, $cur_len, $start, $data) = (0, 0, 0, "");
+        my $digest;
 
         &dprint("Syncing file object: $name\n");
         foreach my $origin ($self->origin()) {
-            if (-f $origin) {
+            my @statinfo = ((S_ISLNK($origin)) ? (lstat($origin)) : (stat($origin)));
+
+            if (S_ISREG($self->filetype()) && -f _) {
                 if (open(FILE, $origin)) {
                     &dprint("   Including file to sync: $origin\n");
-                    while(my $line = <FILE>) {
+                    while (my $line = <FILE>) {
                         $data .= $line;
                     }
                     close FILE;
                 } else {
                     &wprint("Could not open origin path ($origin) for file object '$name'\n");
                 }
+            } elsif (S_ISLNK($self->filetype())) {
+                if (-l _) {
+                    $data = readlink($origin);
+                } else {
+                    $data = $origin;
+                }
+            } elsif (S_ISBLK($self->filetype()) && -b _) {
+                $data = $statinfo[6];
+            } elsif (S_ISCHR($self->filetype()) && -c _) {
+                $data = $statinfo[6];
+            } else {
+                $data = "";
             }
         }
 
@@ -363,6 +409,11 @@ file_import()
 {
     my ($self, $path) = @_;
     my $id = $self->id();
+    my @statinfo;
+    my $db = Warewulf::DataStore->new();
+    my $binstore = $db->binstore($id);
+    my ($import_size, $format) = (0, "");
+    my ($format, $buffer, $digest);
     local *FILE;
 
     if (! $id) {
@@ -374,56 +425,79 @@ file_import()
 
     if ($path =~ /^(\/.+)$/) {
         $path = $1;
-        if (-f $path) {
-            my $db = Warewulf::DataStore->new();
-            my $binstore = $db->binstore($id);
-            my $import_size = 0;
-            my ($format, $buffer, $digest);
-
-            if (open(FILE, $path)) {
-                my $length;
-
-                $digest = Digest::MD5->new();
-                while ($length = sysread(FILE, $buffer, $db->chunk_size())) {
-                    if ($import_size == 0) {
-                        if ($buffer =~ /^#!\/bin\/sh/) {
-                            $format = "shell";
-                        } elsif ($buffer =~ /^#!\/bin\/bash/) {
-                            $format = "bash";
-                        } elsif ($buffer =~ /^#!\/[a-zA-Z0-9\/_\.]+\/perl/) {
-                            $format = "perl";
-                        } elsif ($buffer =~ /^#!\/[a-zA-Z0-9\/_\.]+\/python/) {
-                            $format = "python";
-                        } else {
-                            $format = "data";
-                        }
-                    }
-                    &dprint("Chunked $length bytes of $path\n");
-                    $binstore->put_chunk($buffer);
-                    $digest->add($buffer);
-                    $import_size += $length;
-                }
-                if (!defined($length) && (! $import_size)) {
-                    eprint("Unable to import $path:  $!\n");
-                    return undef;
-                }
-                close FILE;
-
-                $self->size($import_size);
-                $self->checksum($digest->hexdigest());
-                $self->format($format);
-                $db->persist($self);
-                return $import_size;
-            } else {
-                &eprint("Could not open file: $!\n");
-            }
-        } else {
-            &eprint("File not found: $path\n");
-        }
     } else {
         &eprint("Import filename contains illegal characters.\n");
+        return undef;
     }
-    return undef;
+
+    @statinfo = lstat($path);
+    $digest = Digest::MD5->new();
+    if (! -e _) {
+        &eprint("File not found: $path\n");
+        return undef;
+    } elsif (-f _) {
+        my $length;
+
+        if (!open(FILE, $path)) {
+            &eprint("Could not open import file \"$path\" for reading:  $!\n");
+            return undef;
+        }
+        while ($length = sysread(FILE, $buffer, $db->chunk_size())) {
+            if ($import_size == 0) {
+                if ($buffer =~ /^#!\/bin\/sh/) {
+                    $format = "shell";
+                } elsif ($buffer =~ /^#!\/bin\/bash/) {
+                    $format = "bash";
+                } elsif ($buffer =~ /^#!\/[a-zA-Z0-9\/_\.]+\/perl/) {
+                    $format = "perl";
+                } elsif ($buffer =~ /^#!\/[a-zA-Z0-9\/_\.]+\/python/) {
+                    $format = "python";
+                } else {
+                    $format = "data";
+                }
+            }
+            &dprint("Chunked $length bytes of $path\n");
+            $binstore->put_chunk($buffer);
+            $digest->add($buffer);
+            $import_size += $length;
+        }
+        if (!defined($length) && (! $import_size)) {
+            eprint("Unable to import $path:  $!\n");
+            return undef;
+        }
+        close FILE;
+    } elsif (-l _) {
+        my $target = readlink($path);
+
+        &dprint("Importing symlink:  $path -> $target\n");
+        $binstore->put_chunk($target);
+        $digest->add($target);
+        $import_size += length($target);
+    } elsif (-b _ || -c _) {
+        my ($major, $minor) = ($statinfo[6] >> 8, $statinfo[6] & 0xff);
+
+        &dprintf("Importing %s special device $path:  0x%02x (%d), 0x%02x (%d)\n",
+                 ((-b _) ? ("block") : ("character")), $major, $major, $minor, $minor);
+        $binstore->put_chunk($statinfo[6]);
+        $digest->add($statinfo[6]);
+        $import_size += length($statinfo[6]);
+    } else {
+        &dprintf("Importing %s $path\n",
+                 ((S_ISDIR($statinfo[2]))
+                  ? ("directory")
+                  : ((S_ISFIFO($statinfo[2]))
+                     ? ("FIFO (named pipe)")
+                     : ((S_ISSOCK($statinfo[2]))
+                        ? ("UNIX socket")
+                        : ("<unknown type>")))));
+    }
+    $self->mode($statinfo[2]);
+    $self->filetype($statinfo[2]);
+    $self->size($import_size);
+    $self->checksum($digest->hexdigest());
+    $self->format($format);
+    $db->persist($self);
+    return $import_size;
 }
 
 
--- provision/cgi-bin/file.pl
+++ provision/cgi-bin/file.pl
@@ -30,202 +30,208 @@ my $tmpdir = "/tmp/warewulf";
 my $hwaddr = $q->param('hwaddr');
 my $fileid = $q->param('fileid');
 my $timestamp = $q->param('timestamp');
-my $node;
+my @nodes;
+my ($node, $oSet, $nodeName, $read_buffer, $send_buffer, $fileObj, $fileID, $fileName, $cachedir, $cachefile);
 
-if (! -d $tmpdir) {
-    mkpath($tmpdir);
-    chmod(0750, $tmpdir);
+if ((! -d $tmpdir) && !mkpath($tmpdir, 0, 0750)) {
+    &eprint("Unable to create temporary directory \"$tmpdir\" -- $!\n");
+    exit(-1);
 }
 
 if ($hwaddr =~ /^([a-zA-Z0-9:]+)$/) {
     $hwaddr = $1;
-
-    my $oSet = $db->get_objects("node", "_hwaddr", $hwaddr);
-    foreach my $tnode ($oSet->get_list()) {
-        if (! $tnode->enabled()) {
-            next;
-        }
-        $node = $tnode;
-    }
-
-    if ($node) {
-        my $nodeName = $node->name();
-
-        if (! $fileid) {
-            my @files = $node->get("fileids");
-
-            print $q->header("text/plain");
-            if (scalar(@files)) {
-                my $objSet = $db->get_objects("file", "_id", @files);
-                my %metadata;
-
-                foreach my $obj ($objSet->get_list()) {
-                    if (ref($obj) ne "Warewulf::File") {
-                        my $fileid = $obj->id();
-                        &wprint("ObjectID ($fileid) is not of type 'Warewulf::File' (metadata request by: $nodeName/$hwaddr)\n");
-                        next;
-                    }
-
-                    if (ref($obj) eq "Warewulf::File") {
-                        my $obj_timestamp = $obj->timestamp() || 0;
-
-                        if ($timestamp and $timestamp >= $obj_timestamp) {
-                            next;
-                        }
-                        $metadata{$obj_timestamp} .= sprintf("%s %s %s %s %04o %s %s\n",
-                            $obj->id() || "NULL",
-                            $obj->name() || "NULL",
-                            $obj->uid() || "0",
-                            $obj->gid() || "0",
-                            $obj->mode() || "0000",
-                            $obj_timestamp,
-                            $obj->path() || "NULL"
-                        );
-                    }
-                }
-                foreach my $t (sort {$a <=> $b} keys %metadata) {
-                    print $metadata{$t};
-                }
-            }
-        } elsif ($fileid =~ /^([0-9]+)$/ ) {
-            $fileid = $1;
-            my $read_buffer;
-            my $send_buffer;
-
-            my $fileObj = $db->get_objects("file", "_id", $fileid)->get_object(0);;
-
-            if ($fileObj) {
-                if (ref($fileObj) eq "Warewulf::File") {
-                    my $fileID = $fileObj->id();
-                    my $cachedir = "$tmpdir/files/$fileID/";
-                    my $cachefile = "$cachedir/". $fileObj->checksum();
-
-                    # Initially cache the file if it doesn't already exist locally
-                    if (! -f $cachefile) {
-                        if (! -d $cachedir) {
-                            mkpath($cachedir);
-                        }
-                        $fileObj->file_export($cachefile);
-                    }
-
-                    # Make sure checksum exists before going forward. Otherwise we will remove cached
-                    # file below, and send an internal error to the client.
-                    if (&digest_file_hex_md5($cachefile) eq $fileObj->checksum()) {
-
-                        if (open(CACHE, $cachefile)) {
-                            while(my $line = <CACHE>) {
-                                $read_buffer .= $line;
-                            }
-                            close CACHE;
-                        }
-
-                        # Search for all matching variable entries.
-                        foreach my $wwstring ($read_buffer =~ m/\%\{[^\}]+\}(?:\[\d+\])?/g) {
-                            # Check for format, and seperate into a seperate wwvar string
-                            if ($wwstring =~ /^\%\{(.+?)\}(\[(\d+)\])?$/) {
-                                my $wwvar = $1;
-                                my $wwarrayindex = $3;
-                                # Set the current object that we are looking at. This is
-                                # important as we iterate through multiple levels.
-                                my $curObj = $node;
-                                my @keys = split(/::/, $wwvar);
-                                while(my $key = shift(@keys)) {
-                                    my $val = $curObj->get($key);
-                                    if (ref($val) eq "Warewulf::ObjectSet") {
-                                        my $find = shift(@keys);
-                                        my $o = $val->find("name", $find);
-                                        if ($o) {
-                                            $curObj = $o;
-                                        } else {
-                                            &dprint("Could not find object: $find\n");
-                                        }
-
-                                    } elsif (ref($val) eq "ARRAY") {
-                                        my $v;
-                                        if ($wwarrayindex) {
-                                            $v = $val->[$wwarrayindex];
-                                        } else {
-                                            $v = $val->[0];
-                                        }
-                                        $read_buffer =~ s/\Q$wwstring\E/$v/g;
-                                    } elsif ($val) {
-                                        $read_buffer =~ s/\Q$wwstring\E/$val/g;
-                                    } else {
-                                        $read_buffer =~ s/\Q$wwstring\E//g;
-                                    }
-                                }
-                            }
-                        }
-
-                        if ($fileObj->interpreter()) {
-                            my $interpreter = $fileObj->interpreter();
-                            my $pipe_in;
-                            my $pipe_out;
-                            eval {
-                                local $SIG{ALRM} = sub { die "FileID ($fileid) timeout on running intrepreter '$interpreter'\n" };
-                                alarm 1;
-                                my $pid = open2($pipe_out, $pipe_in, "$interpreter");
-                                if ($pid) {
-                                    print $pipe_in $read_buffer;
-                                    close $pipe_in;
-                                    while(my $line = <$pipe_out>) {
-                                        $send_buffer .= $line;
-                                    }
-                                    close $pipe_out;
-                                }
-                                alarm 0;
-                            };
-                            if ($@) {
-                                &eprint("FileID ($fileid) failed running interpreter '$intrepreter'\n");
-                                $send_buffer = undef;
-                            }
-                        } elsif ($read_buffer) {
-                            $send_buffer = $read_buffer;
-                        }
-
-                        if ($send_buffer) {
-                            $q->print("Content-Type: application/octet-stream\r\n");
-                            $q->print("Content-Disposition: attachment\r\n");
-                            $q->print("\r\n");
-
-                            print $send_buffer;
-                        }
-                    } else {
-                        &eprint("FileID ($fileid) cached file checksum does not match bin store, unlinking...\n");
-                        $q->print("Content-Type: application/octet-stream\r\n");
-                        $q->print("Status: 500\r\n");
-                        $q->print("\r\n");
-                        unlink($cachefile);
-                    }
-                } else {
-                    &wprint("ObjectID ($fileid) is not of type 'Warewulf::File' (requested by: $nodeName/$hwaddr)\n");
-                    $q->print("Content-Type: application/octet-stream\r\n");
-                    $q->print("Status: 400\r\n");
-                    $q->print("\r\n");
-                }
-            } else {
-                &wprint("FILEID ($fileid) does not exist (requested by: $nodeName/$hwaddr)\n");
-                $q->print("Content-Type: application/octet-stream\r\n");
-                $q->print("Status: 400\r\n");
-                $q->print("\r\n");
-            }
-        } else {
-            # A file ID was given, but its an invalid ID. This needs to error out client so that
-            # the client doesn't overwrite the target file.
-            &wprint("FILEID ($fileid) contains invalid characters (requested by: $nodeName/$hwaddr)\n");
-            $q->print("Content-Type: application/octet-stream\r\n");
-            $q->print("Status: 404\r\n");
-            $q->print("\r\n");
-        }
-    } else {
-        &wprint("HWADDR ($hwaddr) is undefined\n");
-        $q->print("Content-Type: application/octet-stream\r\n");
-        $q->print("Status: 404\r\n");
-        $q->print("\r\n");
-    }
 } else {
-    &wprint("HWADDR ($hwaddr) contains invalid characters\n");
+    &wprint("HWADDR \"$hwaddr\" contains invalid character(s)\n");
     $q->print("Content-Type: application/octet-stream\r\n");
     $q->print("Status: 404\r\n");
     $q->print("\r\n");
+    exit(0);
+}
+
+$oSet = $db->get_objects("node", "_hwaddr", $hwaddr);
+@nodes = grep { $_->enabled() } $oSet->get_list();
+if (scalar(@nodes) != 1) {
+    &wprintf("HWADDR \"$hwaddr\" not found or not unique (%d found)\n", scalar(@nodes));
+    $q->print("Content-Type: application/octet-stream\r\n");
+    $q->print("Status: 404\r\n");
+    $q->print("\r\n");
+    exit(0);
+}
+
+$nodeName = $node->name();
+if (! $fileid) {
+    my @files = $node->get("fileids");
+
+    print $q->header("text/plain");
+    if (scalar(@files)) {
+        my $objSet = $db->get_objects("file", "_id", @files);
+        my %metadata;
+
+        foreach my $obj ($objSet->get_list()) {
+            my ($obj_id, $obj_name) = ($obj->id(), $obj->name() || "NULL");
+            my ($obj_ts, $obj_uid, $obj_gid);
+
+            if (ref($obj) ne "Warewulf::File") {
+                &wprintf("Object $obj_id ($obj_name) provisioned to $nodeName ($hwaddr) is a(n) %s; should be Warewulf::File!\n",
+                         ref($obj));
+                next;
+            }
+            ($obj_ts, $obj_uid, $obj_gid, $obj_ftype) = ($obj->timestamp(), $obj->uid(), $obj->gid(), $obj->filetypestring());
+
+            if ($timestamp && $timestamp >= $obj_ts) {
+                next;
+            }
+            $metadata{$obj_ts} .= sprintf("$obj_id $obj_name $obj_uid $obj_gid %c%04o $obj_ts %s\n",
+                                          (($obj_ftype eq '-') ? (' ') : ($obj_ftype)),
+                                          $obj->mode(), $obj->path() || "NULL");
+        }
+        print map { $metadata{$_} } sort { $a <=> $b } keys(%metadata);
+    }
+} elsif ($fileid =~ /^([0-9]+)$/ ) {
+    $fileid = $1;
+} else {
+    # A file ID was given, but its an invalid ID.  This needs to error out on
+    # the client so that the client doesn't overwrite the target file.
+    $fileid =~ s/[^[:print:]]//g;
+    &wprint("Requested File ID \"$fileid\" contains invalid characters (requested by: $nodeName/$hwaddr)\n");
+    $q->print("Content-Type: application/octet-stream\r\n");
+    $q->print("Status: 404\r\n");
+    $q->print("\r\n");
+    exit(0);
+}
+
+if (!($fileObj = $db->get_objects("file", "_id", $fileid)->get_object(0))) {
+    &wprint("Requested File ID \"$fileid\" does not exist (requested by: $nodeName/$hwaddr)\n");
+    $q->print("Content-Type: application/octet-stream\r\n");
+    $q->print("Status: 400\r\n");
+    $q->print("\r\n");
+    exit(0);
+} elsif (ref($fileObj) ne "Warewulf::File") {
+    &wprintf("Object $fileid (%s) provisioned to $nodeName ($hwaddr) is a(n) %s; should be Warewulf::File!\n",
+             $fileObj->name(), ref($fileObj));
+    $q->print("Content-Type: application/octet-stream\r\n");
+    $q->print("Status: 400\r\n");
+    $q->print("\r\n");
+    exit(0);
+}
+
+$fileID = $fileObj->id();
+$fileName = $fileObj->name();
+$cachedir = "$tmpdir/files/$fileID/";
+$cachefile = "$cachedir/". $fileObj->checksum();
+
+# Initially cache the file if it doesn't already exist locally
+if (! -f $cachefile) {
+    if (! -d $cachedir && !mkpath($cachedir, 0, 0770)) {
+        &eprint("Unable to create cache directory \"$cachedir\" -- $!\n");
+        exit(-1);
+    }
+    $fileObj->file_export($cachefile);
+}
+
+# Make sure checksum exists before going forward. Otherwise we will remove cached
+# file below, and send an internal error to the client.
+if (&digest_file_hex_md5($cachefile) ne $fileObj->checksum()) {
+    unlink($cachefile);
+    &eprint("File $fileID ($fileName) checksum from data store does not match cached copy; unlinking.\n");
+    $q->print("Content-Type: application/octet-stream\r\n");
+    $q->print("Status: 500\r\n");
+    $q->print("\r\n");
+    exit(-1);
+}
+if (open(CACHE, $cachefile)) {
+    my @statinfo = stat($cachefile);
+
+    if (sysread(CACHE, $read_buffer, $statinfo[7]) < $statinfo[7]) {
+        unlink($cachefile);
+        &eprint("Error reading from cache file $cachefile for file $fileID ($fileName) -- $!; unlinking.\n");
+        $q->print("Content-Type: application/octet-stream\r\n");
+        $q->print("Status: 500\r\n");
+        $q->print("\r\n");
+        exit(-1);
+    }
+    close CACHE;
+} else {
+    unlink($cachefile);
+    &eprint("Error opening cache file $cachefile for file $fileID ($fileName) -- $!; unlinking.\n");
+    $q->print("Content-Type: application/octet-stream\r\n");
+    $q->print("Status: 500\r\n");
+    $q->print("\r\n");
+    exit(-1);
+}
+
+# Search for all matching variable entries.
+foreach my $wwstring ($read_buffer =~ m/\%\{[^\}]+\}(?:\[\d+\])?/g) {
+    # Check for format, and separate into a separate wwvar string
+    if ($wwstring =~ /^\%\{(.+?)\}(\[(\d+)\])?$/) {
+        # Set the current object that we are looking at. This is
+        # important as we iterate through multiple levels.
+        my $curObj = $node;
+        my ($wwvar, $wwarrayindex) = ($1, $3);
+        my @keys = split(/::/, $wwvar);
+
+        while (my $key = shift(@keys)) {
+            my $val = $curObj->get($key);
+
+            if (ref($val) eq "Warewulf::ObjectSet") {
+                my $find = shift(@keys);
+                my $o = $val->find("name", $find);
+
+                if ($o) {
+                    $curObj = $o;
+                } else {
+                    &dprint("Could not find object:  $find\n");
+                }
+            } elsif (ref($val) eq "ARRAY") {
+                my $v;
+
+                if ($wwarrayindex) {
+                    $v = $val->[$wwarrayindex];
+                } else {
+                    $v = $val->[0];
+                }
+                $read_buffer =~ s/\Q$wwstring\E/$v/g;
+            } elsif (defined($val)) {
+                $read_buffer =~ s/\Q$wwstring\E/$val/g;
+            } else {
+                $read_buffer =~ s/\Q$wwstring\E//g;
+            }
+        }
+    }
+}
+
+if ($fileObj->interpreter()) {
+    my $interpreter = $fileObj->interpreter();
+    my ($pipe_in, $pipe_out);
+
+    eval {
+        my $pid;
+        local $SIG{ALRM} = sub { die "File $fileID ($fileName) timed out on running intrepreter '$interpreter'\n" };
+
+        alarm(1);
+        if (($pid = open2($pipe_out, $pipe_in, "$interpreter"))) {
+            print $pipe_in $read_buffer;
+            close $pipe_in;
+            while (my $line = <$pipe_out>) {
+                $send_buffer .= $line;
+            }
+            close $pipe_out;
+            waitpid($pid, 0);
+        }
+        alarm(0);
+    };
+    if ($@) {
+        &eprint("FileID ($fileid) failed running interpreter '$intrepreter'\n");
+        $send_buffer = undef;
+    }
+} elsif ($read_buffer) {
+    $send_buffer = $read_buffer;
+}
+
+if ($send_buffer) {
+    $q->print("Content-Type: application/octet-stream\r\n");
+    $q->print("Content-Disposition: attachment\r\n");
+    $q->print("\r\n");
+
+    print $send_buffer;
 }
